4e61c244919a41f0f255507cf673cc431e27a0d9
saikalpakuriksp-star <saikalpakuriksp@gmail.com>
2026-04-21 10:24:01 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello{
 public static void main (String[] args)
 {
-System.out.println("Hello World from jenkins made some changes : lets check");
+System.out.println("Hello World from jenkins made some changes : lets check.....Oracle Java is the #1 programming language and development platform. It reduces costs, shortens development timeframes, drives innovation, and improves ...
+");
 }
 }
